16c3a833710dc5a3ca528482b4207353eee76d22
yyznm <yyz_nm@163.com>
2026-04-09 11:51:21 +0800

    完善内置插件文档

--- zh/plugin/sidebar.ts
+++ zh/plugin/sidebar.ts
@@ -12,20 +12,20 @@ function sidebar(): DefaultTheme.SidebarItem[] {
         {
             text: '插件', items: [
                 {text: '什么是插件？', link: 'start'},
-                {text: '插件开发', link: 'develop'},
-                {text: '插件安装', link: 'install'},
+                // {text: '插件安装', link: 'install'},
             ]
         },
         {
             text: "内置插件",
             items: [
-                {text: '实时视频', link: ''},
-                {text: '摇杆控制', link: ''},
-                {text: '手柄控制', link: ''},
-                {text: '键盘控制', link: ''},
-                {text: '连接状态', link: ''},
-                {text: '大灯', link: ''},
-                {text: '滑块', link: ''},
+                {text: '实时视频 (Live)', link: 'built-in/live'},
+                {text: '摇杆控制 (Nipple)', link: 'built-in/nipple'},
+                {text: '手柄控制 (Gamepad)', link: 'built-in/gamepad'},
+                {text: '键盘控制 (Keyboard)', link: 'built-in/keyboard'},
+                {text: '连接状态 (Signal)', link: 'built-in/signal'},
+                {text: '滑块 (Slider)', link: 'built-in/slider'},
+                {text: '开关按钮 (SwitchButton)', link: 'built-in/switch-button'},
+                {text: '按钮 (Button)', link: 'built-in/button'},
             ]
         },
         {
